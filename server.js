e2bccc5112d0c149656d967883d0328f23b49b6c
SHargus <87950738+harguss@users.noreply.github.com>
2023-01-28 17:42:47 -0600

    getting a city returned from data

--- server.js
+++ server.js
@@ -2,20 +2,43 @@
 
 const express = require('express');
 require('dotenv').config();
-let data = require('./data/weather.json');
+let weatherData = require('./data/weather.json');
 const cors = require('cors');
-
-console.log('Hello from our SERVER PORT!!!!!', process.env.PORT);
-
 
 
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5005;
 
-// app.get('/hello', (request, response) => {
-//   localhost:3003/hello
+app.get('/weather',(request, response)=>{
 
-//   console.log('request object', request.query);
-  
-// });
+  // http://localhost:3003/weather? city_name=Paris
+  // console.log('req lat', weatherData[0].lat);
+//create var to store request.query.city_name
+let weatherCity = request.query.searchQuery;
+// console.log('city?',weatherCity);
+
+//use find to pull data from json  weatherData.find()
+let weatherDataObject = weatherData.find(day => day.city_name === weatherCity);
+// console.log('objects', weatherDataObject.data);
+//send data to the new Weather class
+let dataToResponse = weatherDataObject.data.map(forecast => new Forecast(forecast))
+// console.log('did we get object back from constructor', dataToResponse);
+//response
+  response.send(dataToResponse);
+
+});
+
+
+
+
+class Forecast{
+  constructor(weatherObject){
+    // console.log('in class ', weatherObject);
+    //date
+    this.date = weatherObject.datetime;
+    //description
+  }
+}
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
