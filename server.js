40df7c5a388b16920807ce1a2c94cb2d15f100a1
SHargus <87950738+harguss@users.noreply.github.com>
2023-02-06 19:26:40 -0600

    pushing for craig

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5005;
 
-// let WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+let WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 app.get('/',(request, response)=>{
   response.send('hello')
 });
@@ -29,30 +29,31 @@ app.get('/',(request, response)=>{
 
   app.get('/weather', async(request,response)=>{
   console.log(request.query.searchQuery);
-    // try {
+    try {
      
-    //   let lat = request.query.latitude;
-    //   let lon = request.query.longitude;
-    //   let weatherCity = request.query.searchQuery;
+      let lat = request.query.latitude;
+      let lon = request.query.longitude;
+      let weatherCity = request.query.searchQuery;
 
       // let weatherDataObject = weatherData.find(day => day.city_name.toLowerCase() === weatherCity.toLowerCase());
-      //  const weatherUrl = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}=London&days=1&aqi=no&alerts=no`
-      //  let weatherInfo = await axios(weatherUrl);
+        const weatherUrl = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&query=${lat},${lon},${weatherCity}&format=json`
+       let weatherInfo = await axios(weatherUrl);
 
-      //  console.log('!!!!!!!!!!!!!xxxxxxx',weatherInfo);
+       console.log('!!!!!!!!!!!!!xxxxxxx',weatherInfo.data);
 
-      // let dataToResponse = weatherInfo.data.map(weatherInfo => new Forecast(weatherInfo));
+      let dataToResponse = weatherInfo.data.map(weatherInfo => new Forecast(weatherInfo));
+      console.log("🚀 ~ file: server.js:45 ~ app.get ~ dataToResponse", dataToResponse)
       response.status(200).send('dataToResponse');
+
       
-    // } catch (error) {
-    //   console.error('ERROR', error);
-    // }
+      
+    } catch (error) {
+      console.error('ERROR', error);
+    }
   }
   );
    // 404 not found path.
-app.use('*', (request, response) => {
-  response.status(404).send('The route was not found. Error 404');
-});
+
 
 class Forecast {
   constructor(weatherObject) {
@@ -63,18 +64,23 @@ class Forecast {
     this.description = weatherObject.weather.description;
   }
 }
-// app.get('/', (request, response) => {
-//   try {
-//     console.log(weatherData);
-//     response.status(200).send('hello from our server HOME route / !!!!!');
-//   } catch (error) {
-//     console.log(error);
-//   }
-//  });
+
+
+app.get('/', (request, response) => {
+  try {
+    console.log(weatherData);
+    response.status(200).send('hello from our server HOME route / !!!!!');
+  } catch (error) {
+    console.log(error);
+  }
+ });
 // class Movie {
 //   constructor(movieObject) {
 //     console.log('')
 //   }
 // }
+app.use('*', (request, response) => {
+  response.status(404).send('The route was not found. Error 404');
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
