5904aca13c948b13e472805fb569b690dbb7b30f
SHargus <87950738+harguss@users.noreply.github.com>
2023-04-11 10:40:18 -0500

    weather and movies working

--- server.js
+++ server.js
@@ -25,11 +25,11 @@ app.get('/movie', async (request, response, next) => {
     const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false&query=${movieSearchQuery}&format=json`;
     let results = await axios.get(movieUrl);
 
-    // let constructorData = results.data;
+    let constructorData = results.data;
     console.log('XXXXXX',constructorData);
     let movieData = constructorData.results.map((movies) => new Movie(movies));
 
-    response.status(200).send('movieData');
+    response.status(200).send(movieData);
   
 
     //     let results = await axios.get(url);
@@ -44,7 +44,7 @@ app.get('/movie', async (request, response, next) => {
 
 
 app.get('/weather', async (request, response) => {
-  // console.log(request.query.searchQuery);
+  console.log(request.query.searchQuery);
   try {
 
     let lat = request.query.latitude;
@@ -80,15 +80,15 @@ class Forecast {
   }
 }
 
-class Movies {
+class Movie {
   constructor(movieObject) {
     console.log("🚀 ~ file: server.js:94 ~ Movies ~ constructor ~ movieObject", movieObject)
   
-    this.movieObject = movieObject.d;
+   
     this.tableName = 'movies';
-    this.title = movie.title;
-    this.overview = movie.overview;
-    this.imageUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+    this.title = movieObject.title;
+    this.overview = movieObject.overview;
+    this.imageUrl = 'https://image.tmdb.org/t/p/w500' + movieObject.poster_path;
     this.timestamp = Date.now();
   }
 }
